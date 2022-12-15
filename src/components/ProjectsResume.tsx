05ad752e1cf986caf2a2a6ea97e4975413537179
nnnnadia <tristaonadia@gmail.com>
2022-12-15 14:06:12 -0300

    feat: tinkle project card's layout

--- src/components/ProjectsResume.tsx
+++ src/components/ProjectsResume.tsx
@@ -1,4 +1,5 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Grid, Link, Typography } from '@mui/material';
+import { Button, ButtonGroup, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, Typography } from '@mui/material';
+import { Stack } from '@mui/system';
 import React, { useState } from 'react';
 import projectService from '../service/projectService';
 
@@ -9,37 +10,33 @@ export default function ProjectsResume() {
     <Grid container justifyContent="center" alignItems="center" spacing={4}>
       {projects.map((project) => <Grid item key={project.id}>
         <Card sx={{ maxWidth: 345 }}>
-          <CardMedia
-            component="img"
-            height="140"
-            image={project.printUrl}
-            alt="project's print screen"
-          />
+          <CardActionArea href={project.projectUrl}>
+            <CardMedia
+              component="img"
+              height="140"
+              image={project.printUrl}
+              alt="project's print screen"
+            />
+          </CardActionArea>
           <CardContent>
             <Typography variant="h5" component="div">
               {project.title}
             </Typography>
-            <Typography variant="body2" color="text.secondary">
+            <Typography variant="body2" color="text.secondary" sx={{ height: '6em' }}>
               {project.description}
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size="small">
-              <Link
-                href={project.projectUrl}
-                underline="none"
-              >
-                Projeto
-              </Link>
-            </Button>
-            <Button size="small">
-              <Link
-                href={project.repositoryUrl}
-                underline="none"
-              >
-                Repositório
-              </Link>
-            </Button>
+            <Stack direction="row" width="100%" justifyContent="center">
+              <ButtonGroup variant="outlined">
+                <Button size="small" href={project.projectUrl}>
+                  Projeto
+                </Button>
+                <Button size="small" href={project.repositoryUrl}>
+                  Repositório
+                </Button>
+              </ButtonGroup>
+            </Stack>
           </CardActions>
         </Card>
       </Grid>)}
