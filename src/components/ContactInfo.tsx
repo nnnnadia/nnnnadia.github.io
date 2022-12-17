cf7c52a0ed8d23df2fae55ed4c55e396c8dbfc56
nnnnadia <tristaonadia@gmail.com>
2022-12-17 16:01:01 -0300

    feat: add WhatsApp contact info

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -1,4 +1,4 @@
-import { Box, ButtonGroup, IconButton, Paper, Stack, Typography } from '@mui/material';
+import { Box, ButtonGroup, IconButton, Paper, Popover, Stack, Typography } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
@@ -7,6 +7,18 @@ import TelegramIcon from '@mui/icons-material/Telegram';
 import React from 'react';
 
 export default function ContactInfo() {
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  }
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
   return (
     <Paper
       sx={{
@@ -35,9 +47,25 @@ export default function ContactInfo() {
           <IconButton href="https://www.linkedin.com/in/nadiadutra/">
             <LinkedInIcon fontSize="large" sx={{ color: 'white' }} />
           </IconButton>
-          <IconButton>
+          <IconButton aria-describedby={id} onClick={handleClick}>
             <WhatsAppIcon fontSize="large" sx={{ color: 'white' }} />
           </IconButton>
+          <Popover
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            onClickCapture={handleClose}
+            anchorOrigin={{
+              vertical: 'center',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+          >
+            <img alt="WhatsApp contact QR code" src="https://github.com/nnnnadia/nnnnadia.github.io/blob/main/src/images/whatsapp-contact-qr.jpeg?raw=true" width="100em" height="100em" />
+          </Popover>
           <IconButton>
             <TelegramIcon fontSize="large" sx={{ color: 'white' }} />
           </IconButton>
