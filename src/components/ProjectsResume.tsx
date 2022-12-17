e92c00fce04c770a58fd883c5abf75e3bf9a4853
nnnnadia <tristaonadia@gmail.com>
2022-12-17 11:21:01 -0300

    feat: projects now order by age

--- src/components/ProjectsResume.tsx
+++ src/components/ProjectsResume.tsx
@@ -17,38 +17,41 @@ export default function ProjectsResume() {
 
   return (
     <Grid container justifyContent="center" alignItems="center" spacing={4}>
-      {projects.map((project) => <Grid item key={project.id} hidden={!isProjectStackSelected(project.id)}>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardActionArea href={project.projectUrl}>
-            <CardMedia
-              component="img"
-              height="140"
-              image={project.printUrl}
-              alt="project's print screen"
-            />
-          </CardActionArea>
-          <CardContent>
-            <Typography variant="h5" component="div">
-              {project.title}
-            </Typography>
-            <Typography variant="body2" color="text.secondary" sx={{ height: '6em' }}>
-              {project.description}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Stack direction="row" width="100%" justifyContent="center">
-              <ButtonGroup variant="outlined">
-                <Button size="small" href={project.projectUrl} disabled={!project.projectUrl}>
-                  Projeto
-                </Button>
-                <Button size="small" href={project.repositoryUrl}>
-                  Repositório
-                </Button>
-              </ButtonGroup>
-            </Stack>
-          </CardActions>
-        </Card>
-      </Grid>)}
+      {projects
+        .sort((a, b) => Date.parse(b.conclusionDate) - Date.parse(a.conclusionDate))
+        .map((project) => <Grid item key={project.id} hidden={!isProjectStackSelected(project.id)}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea href={project.projectUrl}>
+              <CardMedia
+                component="img"
+                height="140"
+                image={project.printUrl}
+                alt="project's print screen"
+              />
+            </CardActionArea>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {project.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary" sx={{ height: '6em' }}>
+                {project.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Stack direction="row" width="100%" justifyContent="center">
+                <ButtonGroup variant="outlined">
+                  <Button size="small" href={project.projectUrl} disabled={!project.projectUrl}>
+                    Projeto
+                  </Button>
+                  <Button size="small" href={project.repositoryUrl}>
+                    Repositório
+                  </Button>
+                </ButtonGroup>
+              </Stack>
+            </CardActions>
+          </Card>
+        </Grid>)
+      }
     </Grid>
   );
 }
