ca3bb6af424b77ffb41aba34569e5b1ec026059c
nnnnadia <tristaonadia@gmail.com>
2022-12-31 17:14:42 -0300

    feat: add trybewallet project's urls

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,7 +1,6 @@
 import { Stack } from '@mui/material';
 import React from 'react';
 import ContactInfo from './ContactInfo';
-import PersonalInfo from './PersonalInfo';
 
 export default function AboutMe() {
   return (
